5961ec9cb59f445a8ac4fb81acccf3eb9eb6ca98
Alexandr Osipov <sas.smail.sas@gmail.com>
2017-06-13 14:21:20 +0300

    refactoring and add comments for bubble sort

--- sort/bubble/bubble_sort.go
+++ sort/bubble/bubble_sort.go
@@ -4,14 +4,12 @@ import(
   "github.com/sasZilla/algoritms-on-golang/utils"
 )
 
-func Sort(arr []int, direction string) {
-  arr_len := len(arr)
-  for i := 0; i < arr_len - 1; i++ {
-    for j := i + 1; j < arr_len; j++ {
-      if direction == "asc" && arr[i] > arr[j] {
-         utils.Swap(arr, i, j)
-      }
-      if direction == "desc" && arr[i] < arr[j] {
+// BubbleSort algoritm has complexity O(n*n)
+func BubbleSort(arr []int) {
+  arrLen := len(arr)
+  for i := 0; i < arrLen - 1; i++ {
+    for j := i + 1; j < arrLen; j++ {
+      if arr[i] > arr[j] {
         utils.Swap(arr, i, j)
       }
     }
